4f208e64dd43943ab0a3ddc8f82415815e72b8f1
Mame-Penda <mamependasadio1@gmail.com>
2025-03-25 21:15:32 +0100

    class Currency

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,29 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  static checkString(str, propertyName) {
+    if (typeof str !== 'string') {
+      throw new TypeError(`${propertyName} must be a string`);
+    }
+    return str;
+  }
+
+  set code(newCode) {
+    this._code = Currency.checkString(newCode, 'code');
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(newName) {
+    this._name = Currency.checkString(newName, 'name');
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
