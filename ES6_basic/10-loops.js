990a712c39e85388f4569831458b96c651b1d192
Mame-Penda <mamependasadio1@gmail.com>
2025-03-25 13:39:31 +0100

    For...of Loops

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const array = [];
-    for (const idx of array) {
-      array.push(appendString + idx);
-    }
-  
-    return array;
+  const newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
   }
+
+  return newArray;
+}
