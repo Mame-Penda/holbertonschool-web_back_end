d61df0a0408c73d26a4bc9c6ebff291b4054e902
Mame-Penda <mamependasadio1@gmail.com>
2025-04-01 14:47:12 +0200

    array list

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -4,6 +4,6 @@ export default function groceriesList() {
   list.set('Tomatoes', 10);
   list.set('Pasta', 1);
   list.set('Rice', 1);
-  list.set('Banane', 5);
+  list.set('Banana', 5);
   return list;
 }
