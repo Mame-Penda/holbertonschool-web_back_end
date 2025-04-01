4ebb90011b9767f427df2d9621a4f3a97560927d
Mame-Penda <mamependasadio1@gmail.com>
2025-04-01 14:45:37 +0200

    fixed error

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const newGrade = newGrades.find((grade) => grade.studentId === student.id)[0];
+      const newGrade = newGrades.find((grade) => grade.studentId === student.id);
       const grade = !newGrade ? newGrade.grade : 'N/A';
       return { ...student, grade };
     });
