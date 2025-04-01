f97f069adf1ed45cc5513319f25f7aedb547fd52
Mame-Penda <mamependasadio1@gmail.com>
2025-04-01 14:40:53 +0200

    array items unique

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter((students) => students.location === city)
+    .filter((student) => student.location === city)
     .map((student) => {
-      const newGrade = newGrades.find((grades) => grades.studentId === student.id)[0];
+      const newGrade = newGrades.find((grade) => grade.studentId === student.id)[0];
       const grade = !newGrade ? newGrade.grade : 'N/A';
       return { ...student, grade };
     });
