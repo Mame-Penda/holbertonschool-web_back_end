bc36ecd64d5c2fa265f1114fbe5e01dbddb554b3
Mame-Penda <mamependasadio1@gmail.com>
2025-04-02 13:53:48 +0200

    fixed error

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(fileName) {
   return Promise.reject(
-    new Error(`${fileName} cannot be proccessed`),
+    new Error(`${fileName} cannot be processed`),
   );
 }
