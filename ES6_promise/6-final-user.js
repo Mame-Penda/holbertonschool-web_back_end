e0aeee07b1f6dfef463ec898ca368d168de46063
Mame-Penda <mamependasadio1@gmail.com>
2025-04-02 13:57:43 +0200

    fixed error

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,6 +5,6 @@ export default async (firstName, lastName, fileName) => {
   const value = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
   return value.map((promise) => ({
     status: promise.status,
-    value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
+    value: promise.status === 'fulfilled' ? promise.value : promise.reason.toString(),
   }));
 };
