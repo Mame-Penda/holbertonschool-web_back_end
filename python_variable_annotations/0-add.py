7a898c53f18f3177132b2fd14e450b71675715fe
Mame-Penda <mamependasadio1@gmail.com>
2025-04-23 13:20:50 +0200

    function that returns the sum as a float

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env pyton3
 
-"""function that returns their sum as a float."""
+"""Module that provides a function that returns their sum as a float."""
 
 
 def add(a: float, b: float) -> float:
     """type-annotated function add that takes a float a and a float b
-    and returns their sum as a float."""
+    and returns their sum as a float.
+
+    Args:
+    a (float): First number
+    b(float): Second number
+
+    Returns:
+    float: The sum of a and b
+    """
 
     return a + b
