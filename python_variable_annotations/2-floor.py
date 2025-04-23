a32a7deadc9be6f29ef83ff27491bf1745a22493
Mame-Penda <mamependasadio1@gmail.com>
2025-04-23 13:31:44 +0200

    function that returns the floor of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Module that provides a function that returns the floor of a float."""
+
+import math
+
+
+def concat(n: float) -> int:
+    """
+
+    Args:
+    n: float
+
+    Returns:
+    floor of n
+    """
+
+    return math.floor(n)
