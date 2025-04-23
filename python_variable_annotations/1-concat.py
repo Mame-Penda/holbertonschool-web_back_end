77450ba1970cf0b87a73aff57e27a45766334bcd
Mame-Penda <mamependasadio1@gmail.com>
2025-04-23 13:26:37 +0200

    function that returns two concatenated strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Module that provides a function that returns two concatenated strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """type-annotated function add that takes a float a and a float b
+    and returns their sum as a float.
+
+    Args:
+    str1: str
+    str2: str
+
+    Returns:
+    str1 + str2
+    """
+
+    return str1 + str2
