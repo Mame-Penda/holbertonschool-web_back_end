0ddc87397a6bea310b82444c50a89efdfa97ff30
Mame-Penda <mamependasadio1@gmail.com>
2025-04-23 13:42:14 +0200

    function that returns the strings of a float

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/ env pyton3
+
+
+"""Define and annotate the following variables with the specified values.
+"""
+
+a: int = 1
+pi: float = 3.14
+school: str = "Holberton"
+i_understand_annotations: bool = True
