c3a17581d3dc6ea30cee5505dd2053ab1a0984bc
Mame-Penda <mamependasadio1@gmail.com>
2025-04-23 13:48:05 +0200

    function that returns the strings of a float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""Module provides a function that returns the sum float."""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+
+    Args:
+    input_list:
+
+    Returns:
+    sum: float
+    """
+
+    return float(sum(input_list))
