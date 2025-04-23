db8c1dd538283b22e5cf50a6bd6aec9825dd067a
Mame-Penda <mamependasadio1@gmail.com>
2025-04-23 13:38:07 +0200

    function that returns the strings of a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Module provides a function that returns the string representation float."""
+
+
+def to_str(n: float) -> str:
+    """
+
+    Args:
+    n: float
+
+    Returns:
+    string representation of n
+    """
+
+    return str(n)
