057db442356036172a8295c3e3accbc08f4d7da3
Mame-Penda <mamependasadio1@gmail.com>
2025-04-24 22:20:46 +0200

    measure runtime

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""execute multiple coroutines at the same time with async"""
+"""execute multiple tasks using task_wait_random"""
 
 import asyncio
 from typing import List
-task_wait_random = __import__('4-tasks').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
