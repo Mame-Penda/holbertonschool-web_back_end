ba866f70ed9cb9ef00459b39ce1a925f82b0bdcc
Mame-Penda <mamependasadio1@gmail.com>
2025-04-24 14:55:42 +0200

    asynchrous coroutine that takes in an integer

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,14 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """wait a random beetween 0 and max_delay second
+
+    Args:
+    max_delay (int): max_delay in second
+
+    Returns:
+    float: the delay actually waited
+    """
 
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
