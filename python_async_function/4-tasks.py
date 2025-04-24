dfaafcf8578163bd3d80eb7927db1a66655b80db
Mame-Penda <mamependasadio1@gmail.com>
2025-04-24 22:18:19 +0200

    measure runtime

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -3,11 +3,11 @@
 
 import asyncio
 from typing import List
-wait_random = __import__('4-tasks').task_wait_random
+task_wait_random = __import__('4-tasks').task_wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """return the results of all delay (float values)"""
-    delay = [wait_random(max_delay) for _ in range(n)]
+    delay = [task_wait_random(max_delay) for _ in range(n)]
     results = await asyncio.gather(*delay)
     return sorted(results)
