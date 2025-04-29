7d0c2c7a22d0b3b7918d87d5cc408da931262514
Mame-Penda <mamependasadio1@gmail.com>
2025-04-29 21:14:44 +0200

    async generator

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """coroutine called async_generator that takes no arguments."""
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Coroutine collect 10 random numbers using async_comprehension"""
-    return [number async for number in async_generator()]
+    randoms = [number async for number in async_generator()]
+    return randoms
