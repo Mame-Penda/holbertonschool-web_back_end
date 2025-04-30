39c0738942587540d25ebc207b081029d3702549
Mame-Penda <mamependasadio1@gmail.com>
2025-05-01 00:07:51 +0200

    Simple pagination

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Module for funcyion index_range"""
+"""Module for index_range function and Server class"""
 import csv
 import math
 from typing import List, Dict
 
 
 class Server:
-    """the class Server to paginate"""
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -14,12 +14,12 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-                self.__dataset = dataset[1:]
+            self.__dataset = dataset[1:]
 
         return self.__dataset
 
