3fca431dec828283c5713dbd99976a0175b61718
Mame-Penda <mamependasadio1@gmail.com>
2025-05-02 15:06:19 +0200

    fixed erreor of the code

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -2,5 +2,12 @@
 
 
 def insert_school(mongo_collection, **kwargs):
-    """inserts a new document in a collection based on kwargs"""
-    return mongo_collection.insert_one(kwargs).inserted_id
+    """inserts a new document in a collection based on kwargs
+    Args:
+    mongo_collection: the pymongo collection object
+    **kwargs: Arbitrary keyword arguments
+    Returns:
+    The new _id of the inserted document
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
