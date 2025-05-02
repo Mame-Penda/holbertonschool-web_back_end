08f529a622282a3fa009079510db6267eed4ad0f
Mame-Penda <mamependasadio1@gmail.com>
2025-05-02 15:21:31 +0200

    fixed error

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,25 +3,19 @@
 from pymongo import MongoClient
 
 
-def log_stats():
-    """provide some stats about Nginx logs stored in MongoDB"""
-    client = MongoClient('localhost', 27017)
-    db = client.logs
-    collection = db.nginx
-
-    logs_qty = collection.count_documents({})
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    status_check = collection.count_documents({
-        "method": "GET",
-        "path": "/status"
-    })
-
-    print(f"{logs_qty} logs")
-    print("Methods:")
-    for method in methods:
-        method_qty = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {method_qty}")
-
-
 if __name__ == "__main__":
-    log_stats()
+    client = MongoClient("mongodb:// 127.0.0.1:27017")
+    collection = client.logs.nginx
+
+    log_count = collection.count_documents({})
+    print(f"{log_count} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    status_check_count = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+        )
+    print(f"{status_check_count} status check")
