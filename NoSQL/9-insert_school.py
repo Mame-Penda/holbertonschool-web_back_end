12a603f3287f0c0f384180d24ef53597a5c19097
Mame-Penda <mamependasadio1@gmail.com>
2025-05-02 15:26:57 +0200

    fiexed error

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
+""" Insert a document in a MongoDB collection """
 
 
 def insert_school(mongo_collection, **kwargs):
-    """inserts a new document in a collection based on kwargs
+    """Inserts a new document in the given collection using kwargs
     Args:
-    mongo_collection: the pymongo collection object
-    **kwargs: Arbitrary keyword arguments
+        mongo_collection: The pymongo collection object
+        **kwargs: Arbitrary keyword arguments representing the document fields
     Returns:
-    The new _id of the inserted document
+        The _id of the newly inserted document
     """
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
