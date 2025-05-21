9935f01209d23ac1a12e1ab93c5a18c055523801
Mame-Penda <mamependasadio1@gmail.com>
2025-05-21 22:32:27 +0200

    Handles the http file

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    res.setHeader(200, {'Content-Type': 'text/plain; charset=utf-8'});
+    res.writeHeader(200, { 'Content-Type': 'text/plain; charset=utf-8' });
     res.end('Hello Holberton School!');
 });
 
