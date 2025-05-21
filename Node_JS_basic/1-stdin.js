397ae0f4f50f069ae2c5f0480d76576bd19b745a
Mame-Penda <mamependasadio1@gmail.com>
2025-05-21 20:32:58 +0200

    Implemented the stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,12 @@
-const std = require('process');
-
-
 console.log('Welcome to Holberton School, what is your name?\n');
 
-std.stdin.on('readable', () => {
+process.stdin.on('readable', () => {
     const name = std.stdin.read();
     if (name) {
-        std.stdout.write(`Your name is: ${name}`);
+        process.stdout.write(`Your name is: ${name.toString()}`);
     }
 });
 
-std.stdin.on('end', () => {
+process.stdin.on('end', () => {
     console.log('This important software is now closing');
 });
