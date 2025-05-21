2da0cd4a8fae2dcc0500fb161534d9ddc2b148ba
Mame-Penda <mamependasadio1@gmail.com>
2025-05-21 22:29:38 +0200

    Handles the http file

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,11 +1,14 @@
 const http = require('http');
 
+const port = 1245;
+
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader(200, {'Content-Type': 'text/plain; charset=utf-8'});
     res.end('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
 
 module.exports = app;
