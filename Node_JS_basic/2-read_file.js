d5a564c1b1921795addc0ae69c702e32465dea48
Mame-Penda <mamependasadio1@gmail.com>
2025-05-21 15:52:03 +0200

    Handles the code

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,20 +4,17 @@ function countStudents(path) {
     try {
         const data = fs.readFileSync(path, 'utf-8').trim().split('\n');
         const students = data.filter(line => line.trim() !== '').slice(1);
-
+        
         console.log(`Number of students: ${students.length}`);
-
         const fields = {};
-
         students.forEach((line) => {
             const [firstname, , , field] = line.split(',');
-
+            
             if (!fields[field]) {
                 fields[field] = [];
             }
             fields[field].push(firstname);
         });
-
         for (const [field, names] of Object.entries(fields)) {
             console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
         }
